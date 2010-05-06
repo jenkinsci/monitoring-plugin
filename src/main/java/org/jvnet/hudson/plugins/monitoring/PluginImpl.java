f2e061fbc0aa800685d2c26805f661ad18958574
kohsuke <kohsuke@71c3de6d-444a-0410-be80-ed276b4c234a>
2010-05-06 00:37:23 +0000

    don't rely on HUDSON_HOME.

--- src/main/java/org/jvnet/hudson/plugins/monitoring/PluginImpl.java
+++ src/main/java/org/jvnet/hudson/plugins/monitoring/PluginImpl.java
@@ -1,7 +1,10 @@
 package org.jvnet.hudson.plugins.monitoring;
 
 import hudson.Plugin;
+import hudson.model.Hudson;
 import hudson.util.PluginServletFilter;
+
+import java.io.File;
 
 /**
  * Entry point of the plugin.
@@ -18,20 +21,20 @@ public class PluginImpl extends Plugin {
   public void start() throws Exception {
     super.start();
 	
-	// on active les actions systèmes (gc, heap dump, histogramme mémoire, processus...), sauf si l'administrateur a dit différemment
+	// on active les actions syst?mes (gc, heap dump, histogramme m?moire, processus...), sauf si l'administrateur a dit diff?remment
 	if (System.getProperty("javamelody.system-actions-enabled") == null) {
 		System.setProperty("javamelody.system-actions-enabled", "true");
 	}
-	// on désactive les graphiques jdbc et statistiques sql puisqu'il n'y en aura pas
+	// on d?sactive les graphiques jdbc et statistiques sql puisqu'il n'y en aura pas
 	if (System.getProperty("javamelody.no-database") == null) {
 		System.setProperty("javamelody.no-database", "true");
 	}
-	// le répertoire de stockage est dans le répertoire de hudson au lieu d'être dans le répertoire temporaire
-	// ("/" initial nécessaire sous windows pour javamelody v1.8.1)
-	if (System.getProperty("javamelody.storage-directory") == null && System.getenv("HUDSON_HOME") != null) {
-		System.setProperty("javamelody.storage-directory", "/" + System.getenv("HUDSON_HOME") + "/monitoring");
+	// le r?pertoire de stockage est dans le r?pertoire de hudson au lieu d'?tre dans le r?pertoire temporaire
+	// ("/" initial n?cessaire sous windows pour javamelody v1.8.1)
+	if (System.getProperty("javamelody.storage-directory") == null) {
+		System.setProperty("javamelody.storage-directory", "/" + new File(Hudson.getInstance().getRootDir(),"monitoring").getAbsolutePath());
 	}
-	// google-analytics pour connaître le nombre d'installations actives et pour connaître les fonctions les plus utilisées
+	// google-analytics pour conna?tre le nombre d'installations actives et pour conna?tre les fonctions les plus utilis?es
 	if (System.getProperty("javamelody.analytics-id") == null) {
 		System.setProperty("javamelody.analytics-id", "UA-1335263-7");
 	}
