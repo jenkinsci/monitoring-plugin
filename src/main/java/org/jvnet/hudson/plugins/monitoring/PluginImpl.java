b73fe6e19782481e997d3f4c09ec61e0afefab39
evernat <evernat@free.fr>
2010-05-09 22:06:11 +0000

    major reduction of disk usage by aggregation of some common http requests (use of the javamelody.http-transform-pattern parameter and clean-up at first startup)

--- src/main/java/org/jvnet/hudson/plugins/monitoring/PluginImpl.java
+++ src/main/java/org/jvnet/hudson/plugins/monitoring/PluginImpl.java
@@ -3,6 +3,7 @@ package org.jvnet.hudson.plugins.monitoring;
 import hudson.Plugin;
 import hudson.model.Hudson;
 import hudson.util.PluginServletFilter;
+import javax.servlet.ServletContext;
 
 import java.io.File;
 
@@ -16,31 +17,45 @@ import java.io.File;
  * @author Emeric Vernat
  */
 public class PluginImpl extends Plugin {
+  private ServletContext context;
 
+  @Override
+  public void setServletContext(ServletContext context) {
+    this.context = context;
+  }
+  
   @Override
   public void start() throws Exception {
     super.start();
 	
-	// on active les actions syst?mes (gc, heap dump, histogramme m?moire, processus...), sauf si l'administrateur a dit diff?remment
+	// on active les actions systemes (gc, heap dump, histogramme memoire, processus...), sauf si l'administrateur a dit differemment
 	if (System.getProperty("javamelody.system-actions-enabled") == null) {
 		System.setProperty("javamelody.system-actions-enabled", "true");
 	}
-	// on d?sactive les graphiques jdbc et statistiques sql puisqu'il n'y en aura pas
+	// on desactive les graphiques jdbc et statistiques sql puisqu'il n'y en aura pas
 	if (System.getProperty("javamelody.no-database") == null) {
 		System.setProperty("javamelody.no-database", "true");
 	}
-	// le r?pertoire de stockage est dans le r?pertoire de hudson au lieu d'?tre dans le r?pertoire temporaire
-	// ("/" initial n?cessaire sous windows pour javamelody v1.8.1)
+	// le repertoire de stockage est dans le repertoire de hudson au lieu d'etre dans le repertoire temporaire
+	// ("/" initial necessaire sous windows pour javamelody v1.8.1)
 	if (System.getProperty("javamelody.storage-directory") == null) {
 		System.setProperty("javamelody.storage-directory", "/" + new File(Hudson.getInstance().getRootDir(),"monitoring").getAbsolutePath());
 	}
-	// google-analytics pour conna?tre le nombre d'installations actives et pour conna?tre les fonctions les plus utilis?es
+	// google-analytics pour connaitre le nombre d'installations actives et pour connaitre les fonctions les plus utilisees
 	if (System.getProperty("javamelody.analytics-id") == null) {
 		System.setProperty("javamelody.analytics-id", "UA-1335263-7");
 	}
+	// http-transform-pattern pour aggréger les requêtes contenant des parties "dynamiques" comme des numeros des builds,
+	// les fichiers dans job/<name>/site/, javadoc/, ws/, cobertura/, testReport/, violations/file/
+	// ou les utilisateurs dans user/ ou les fichiers dans /static/abcdef123/
+	if (System.getProperty("javamelody.http-transform-pattern") == null) {
+		System.setProperty("javamelody.http-transform-pattern", "/\\d+/|/site/.+|avadoc/.+|/ws/.+|obertura/.+|estReport/.+|iolations/file/.+|/user/.+|/static/\\w+/");
+	}
+	
+	net.bull.javamelody.HttpMonitoringCleaner.cleanIfFirstStart(context);
 	
 	PluginServletFilter.addFilter(new HudsonMonitoringFilter());
 	
-	// TODO on pourrait ajouter un counter de nom job avec les temps de build
+	// TODO on pourrait ajouter un counter avec les temps de build
   }
 }
