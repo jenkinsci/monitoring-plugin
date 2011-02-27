51be7731cba122bbb450ddd24cd0f9a807c40bb8
evernat <evernat@dev.java.net>
2011-02-27 16:53:01 +0000

    for clarification, add license header in java files with the same license as it was already in pom.xml

--- src/main/java/net/bull/javamelody/NodesController.java
+++ src/main/java/net/bull/javamelody/NodesController.java
@@ -1,5 +1,20 @@
 /*
  * Copyright 2008-2011 by Emeric Vernat
+ *
+ *     This file is part of the Monitoring plugin.
+ *
+ * The Monitoring plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Monitoring plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the Monitoring plugin.  If not, see <http://www.gnu.org/licenses/>.
  */
 package net.bull.javamelody;
 
